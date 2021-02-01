9a7fb7697b485ade896afe7507ecd6d771132546
davx1992 <davidsmurafa@gmail.com>
2021-02-01 20:43:29 +0200

    chore: removed dist folder from git

--- dist/constants/identifiers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SERVICE_IDENTIFIER = {
-    APP: Symbol.for("App"),
-};
-exports.default = SERVICE_IDENTIFIER;
-//# sourceMappingURL=identifiers.js.map
